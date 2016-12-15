0a0138abac467dc365c15b181ced90e14d823572
weedmanu <weedmanu@hotmail.com>
2016-12-15 17:38:17 +0100

    modifié :         stream/cam.py 	supprimé :        stream/pic.jpg

--- stream/cam.py
+++ stream/cam.py
@@ -9,8 +9,8 @@ import psutil
 while True:    # boucle infini
 	
 	
-	f = open("/var/www/html/LiveStream/cam.txt", "r") # on ouvre le fichier code.txt
-	g = int(f.read())     # on le lit et le met dans la variable g
+	f = open("/var/www/html/LiveStream/cam.txt", "r") # on ouvre le fichier cam.txt
+	g = int(f.read())     # on le lit et met le resulat dans la variable g
 	f.close()   # on ferme le fichier 
 	
 
@@ -18,17 +18,17 @@ while True:    # boucle infini
 		
 		# on lance la prise de photos.		
 		subprocess.call("/home/pi/stream/./play.sh", shell=True)			
-		# on remet 0 dans le fichier code.txt
+		# on remet 0 dans le fichier cam.txt
 		subprocess.call("sudo cp /home/pi/stream/zero.txt /var/www/html/LiveStream/cam.txt", shell=True)
 		
 	if g == 2:	# si g = 2 donc.
 		
 		# on stop les programmes	
 		subprocess.call("/home/pi/stream/./stop.sh", shell=True)			
-		# on remet 0 dans le fichier code.txt
+		# on remet 0 dans le fichier cam.txt
 		subprocess.call("sudo cp /home/pi/stream/zero.txt /var/www/html/LiveStream/cam.txt", shell=True)
 	
-	time.sleep(2)	# attent 2s avant de reprendre la boucle
+	time.sleep(1)	# attent 1s avant de reprendre la boucle
 
 
 
