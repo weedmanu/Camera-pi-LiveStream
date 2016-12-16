fee800f11cc078421081ec4a726fa8d70c9b963f
weedmanu <weedmanu@hotmail.com>
2016-12-16 05:14:27 +0100

    modifié :         stream/play.sh

--- stream/play.sh
+++ stream/play.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-raspistill --nopreview -w 1024 -h 760 -a 8 -a "Live terrarium %d-%m-%Y %X" -q 40 -o /home/pi/stream/pic.jpg -tl 100 -t 600000 -th 0:0:0 &
+raspistill --nopreview -w 1024 -h 760 -a 8 -a "Live terrarium %d-%m-%Y %X" -q 40 -o /home/pi/stream/pic.jpg -tl 100 -t 600000 -th 0:0:0 >/dev/null 2>&1
 
-LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /home/pi/stream -n pic.jpg" -o "output_http.so -w /usr/local/www" &
+LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /home/pi/stream -n pic.jpg" -o "output_http.so -w /usr/local/www" >/dev/null 2>&1
 
 exit
 
